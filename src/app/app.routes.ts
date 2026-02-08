602efb9bafb770feea50cf12d8830377f41b2d84
JuanVera11 <juanestebanv0612@gmail.com>
2026-02-08 15:06:57 -0500

    interfaz de reportes financieros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/inbox',
-    pathMatch: 'full',
-  },
-  {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  
   {
     path: 'categoria',
     loadComponent: () => import('./pages/categoria/categoria.page').then( m => m.CategoriaPage)
