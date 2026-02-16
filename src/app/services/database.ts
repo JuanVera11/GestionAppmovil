694baa658bc6f14ac1cfc66fa8ea3a1ed2b968b1
Rafael Arango <rafa131842@gmail.com>
2026-02-16 11:20:35 -0500

    Trabajo En clase

---
+++ src/app/services/database.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Capacitor } from '@capacitor/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Database {
+  
+}
