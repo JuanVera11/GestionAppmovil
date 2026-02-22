99da4301eb3d942069cc3f621c9a1359469f09b8
JuanVera11 <juanestebanv0612@gmail.com>
2026-02-22 18:39:38 -0500

    Modelo de Reporte en Models

--- src/app/models/reporte.ts
+++ src/app/models/reporte.ts
@@ -1,2 +1,52 @@
 export class Reporte {
+    private presupuestoTotal: number;
+    private datosActuales: { valor: number }[];
+    private filtroSeleccionado: string;
+
+    constructor(
+        presupuestoTotal: number,
+        datosActuales: { valor: number }[],
+        filtroSeleccionado: string = '7dias'
+    ) {
+        this.presupuestoTotal = presupuestoTotal;
+        this.datosActuales = datosActuales;
+        this.filtroSeleccionado = filtroSeleccionado;
+    }
+
+    get PresupuestoTotal(): number {
+        return this.presupuestoTotal;
+    }
+
+    set PresupuestoTotal(valor: number) {
+        this.presupuestoTotal = valor;
+    }
+
+    get DatosActuales(): { valor: number }[] {
+        return this.datosActuales;
+    }
+
+    set DatosActuales(datos: { valor: number }[]) {
+        this.datosActuales = datos;
+    }
+
+    get FiltroSeleccionado(): string {
+        return this.filtroSeleccionado;
+    }
+
+    set FiltroSeleccionado(filtro: string) {
+        this.filtroSeleccionado = filtro;
+    }
+
+    get TotalGastado(): number {
+        return this.datosActuales.reduce((acumulador, actual) => acumulador + actual.valor, 0);
+    }
+
+    get SaldoRestante(): number {
+        return this.presupuestoTotal - this.TotalGastado;
+    }
+
+    get PorcentajeGastoTotal(): number {
+        if (this.presupuestoTotal === 0) return 0;
+        return Math.round((this.TotalGastado / this.presupuestoTotal) * 100);
+    }
 }
